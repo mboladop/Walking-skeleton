29f120d7387840faaa96bfe91358326f107007a6
Luisa Bolado <mboladop@hotmail.com>
2018-06-22 10:02:55 +0000

    app:posts, accounts installed: pillow, BS

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+# /accounts/urls.py
+from django.urls import path, include
+from .views import login, register, logout, profile
+
+urlpatterns = [
+    path('login', login, name='login'),
+    path('register', register, name='register'),
+    path('logout', logout, name='logout'),
+    path('profile', profile, name='profile'),
+]
